1168e07a97202278b4d312a0a5ae36db86589bbc
bill.lin <bill.lin@staff.nueip.com>
2023-11-19 18:32:08 +0800

    origin:'*'

--- socket-io-routes.js
+++ socket-io-routes.js
@@ -9,10 +9,7 @@ const { Server } = require("socket.io");
 const configureSocketIO = (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: [
-        "https://localhost:8080",
-        "https://linha10.github.io/electronic_signature",
-      ],
+      origin: "*",
       methods: ["GET", "POST"],
     },
   });
