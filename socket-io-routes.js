6789e75df78b3a8dc0e189fe9a20b416bbe1bacd
bill.lin <bill.lin@staff.nueip.com>
2023-11-19 18:36:36 +0800

    s

--- socket-io-routes.js
+++ socket-io-routes.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 const configureSocketIO = (httpServer) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: "*",
+      origin: ["https://localhost:8080", "https://linha10.github.io"],
       methods: ["GET", "POST"],
     },
   });
